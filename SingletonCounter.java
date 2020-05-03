4a7bd9d924ca983aeb29e23db1300ae1da6b8f9e
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2020-05-03 15:04:52 -0700

    Update SingletonCounter.java

--- SingletonCounter.java
+++ SingletonCounter.java
@@ -2,6 +2,20 @@
 
 public class SingletonCounter {
 
+<<<<<<< Updated upstream
 
+=======
+	public static SingletonCounter getInstance() 
+	{
+		return null;
+	}
+
+	public Object getCounter() 
+	{
+		return null;
+	}
+
+	private static final SingletonCounter mSingleton = new SingletonCounter();
+>>>>>>> Stashed changes
 
 }
