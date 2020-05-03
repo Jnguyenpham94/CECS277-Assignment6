26788066f7d97647d83af4e637fb45ad42db473a
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2020-05-03 15:31:03 -0700

    Update ShapeComposite.java

--- ShapeComposite.java
+++ ShapeComposite.java
@@ -1,5 +1,27 @@
 
 
+<<<<<<< Updated upstream
 public class ShapeComposite {
+=======
+    @Override
+    public String display() 
+    {
+        System.out.println("COMPOSITE START:\n");
+        
+        for(ShapeComponent s : mList)
+        {
+            s.display();
+        }
+
+        return  "COMPOSITE END\n"; 
+    }
+
+    public void add(ShapeComponent c) 
+    {
+        mList.add(c);
+	}
+
+    private ArrayList<ShapeComponent> mList = new ArrayList<ShapeComponent>();
+>>>>>>> Stashed changes
 
 }
